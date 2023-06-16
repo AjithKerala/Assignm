effef645bd2db6e2b2dbe1044a4e4037babd2f2c
Ajith <ajithmurali499@gmail.com>
2023-06-16 23:36:46 +0530

    create register and login page full

--- apps/urls.py
+++ apps/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
       path('',views.Homee,name='home'),
       path('register',views.Register,name='register'),
+      path('login',views.Login,name='login'),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def Homee(request):
     return render(request,'Home.html')
 
 def Register(request):
+    if request.method=="POST":
+        fnam=request.POST['fname']
+        sname=request.POST['sname']
+        ussname=request.POST['usname']
+        email=request.POST['Emails']
+        passwo=request.POST['password']
+        passs2=request.POST['password2']
+        age = request.POST['age']
+        if passwo==passs2:
+            if User.objects.filter(username=ussname).exists():
+               messages.info(request,"Alrready added name")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Mail already added")
+            else:
+                user = User.objects.create_user(username=ussname, password=passwo, email=email, first_name=fnam,
+                                                last_name=sname)
+                user.age = age
+                user.save()
+                return redirect('login')
+        else:
+            messages.info("Password missmatch")
+
+
+
+
+
+
     return render(request,'register.html')
+def Login(request):
+    if request.method == "POST":
+        usname = request.POST['username']
+        pawo = request.POST['pass']
+        obj=auth.authenticate(username=usname,password=pawo)
+        if obj is not None:
+            auth.login(request,obj)
+            return redirect('/')
+        else:
+            messages.info(request,"Please check pass username")
+
+    return render(request,'login.html')
